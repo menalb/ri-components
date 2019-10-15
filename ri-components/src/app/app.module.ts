b55bf26e133118e7154b5336da18d0d21a52eb6c
unknown <rowehna@gmail.com>
2019-10-15 16:36:19 +0200

    add todolist component

--- ri-components/src/app/app.module.ts
+++ ri-components/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componenti/login/login.component';
+import { TodolistComponent } from './componenti/todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ri-components/src/app/componenti/todolist/todolist.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+
+  public input1: string;
+  public todos: any;
+  public todoObj: any;
+  
+  @Output() notify = new EventEmitter();
+  constructor() { 
+    this.input1 = '';
+    this.todos = [];
+  }
+
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.input1,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    this.input1 = '';
+    event.preventDefault();
+  }
+
+  ngOnInit() {
+  }
+  notifyEvent(){ 
+    const testo = 'Todo element added';
+    this.notify.emit(testo);
+}
+}
