fb29b998d1fad7edf3635f06324a05693964ab41
unknown <damy32dc@gmail.com>
2019-10-15 16:10:00 +0200

    nuovi componenti

---
+++ ri-components/src/app/componenti/wawawa/wawawa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wawawa',
+  templateUrl: './wawawa.component.html',
+  styleUrls: ['./wawawa.component.css']
+})
+export class WawawaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
